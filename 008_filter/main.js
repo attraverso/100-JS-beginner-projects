84cb7a878cc87e645ac2a29539d6cc1a026beb1f
attraverso <attraverso.dev@gmail.com>
2020-08-25 18:49:39 +0200

    008: refactor using data attributes instead of classes as hooks for the buttons

--- 008_filter/main.js
+++ 008_filter/main.js
@@ -1,50 +1,41 @@
 /* * * BUTTON FILTERS * * */
 
 document.querySelectorAll('.btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    document.querySelectorAll('.card').forEach(card => {
-      card.classList.add('hidden');
-    });
-    if (btn.classList.contains('btn-blue')) {
+  btn.addEventListener('click', (e) => {
+    //hide all cards
+    document.querySelectorAll('.card').forEach(card => card.classList.add('hidden'));
+    //depending on which button was clicked, reveal the relevant cards
+    let btnType = e.target.dataset.filter;
+    if (btnType == 'blue') {
       revealCards('blue')
-    } else if (btn.classList.contains('btn-green')) {
+    } else if (btnType == 'green') {
       revealCards('green')
-    } else if (btn.classList.contains('btn-red')) {
+    } else if (btnType == 'red') {
       revealCards('red')
     } else {
-      document.querySelectorAll('.card').forEach(card => {
-        card.classList.remove('hidden');
-      });
+      document.querySelectorAll('.card').forEach(card => card.classList.remove('hidden'));
     }
   })
 });
 
 function revealCards(type) {
-  document.querySelectorAll('.card[data-card-type=' + type + ']').forEach(card => {
-    card.classList.remove('hidden');
-  });
+  document.querySelectorAll('.card[data-card-type=' + type + ']').forEach(card => card.classList.remove('hidden'));
 }
 
 /* * * SEARCH * * */
 
+//every time a key is pressed, hide all cards, evaluate and show only the ones matching the search
 document.querySelector('input').addEventListener('keyup', () => {
   let userSearch = document.querySelector('input').value.toLowerCase();
   if (userSearch != '') {
     document.querySelectorAll('.card').forEach(card => {
       card.classList.add('hidden');
       let currentCardName = card.firstElementChild.textContent.toLowerCase();
-      console.log('search: ', userSearch);
-      console.log('card: ', currentCardName);
       if (currentCardName.includes(userSearch)) {
-      console.log('true');
-      console.log('and includes says: ', currentCardName.includes(userSearch));
       card.classList.remove('hidden');
       }
     });
   } else {
-    document.querySelectorAll('.card').forEach(card => {
-      card.classList.remove('hidden');
-    });
-    console.log('false');
+    document.querySelectorAll('.card').forEach(card => card.classList.remove('hidden'));
   }
 })
