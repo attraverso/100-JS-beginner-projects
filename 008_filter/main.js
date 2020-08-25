c20a0c8c753ee92e5c56127271cad8198eb1add5
attraverso <attraverso.dev@gmail.com>
2020-08-25 17:33:38 +0200

    008: add working search filtering

--- 008_filter/main.js
+++ 008_filter/main.js
@@ -27,3 +27,24 @@ function revealCards(type) {
 
 /* * * SEARCH * * */
 
+document.querySelector('input').addEventListener('keyup', () => {
+  let userSearch = document.querySelector('input').value.toLowerCase();
+  if (userSearch != '') {
+    document.querySelectorAll('.card').forEach(card => {
+      card.classList.add('hidden');
+      let currentCardName = card.firstElementChild.textContent.toLowerCase();
+      console.log('search: ', userSearch);
+      console.log('card: ', currentCardName);
+      if (currentCardName.includes(userSearch)) {
+      console.log('true');
+      console.log('and includes says: ', currentCardName.includes(userSearch));
+      card.classList.remove('hidden');
+      }
+    });
+  } else {
+    document.querySelectorAll('.card').forEach(card => {
+      card.classList.remove('hidden');
+    });
+    console.log('false');
+  }
+})
